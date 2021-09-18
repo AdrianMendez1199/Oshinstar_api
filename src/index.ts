bc21cf904dd8dfbdf840aa29f91688932406b432
Adrian Mendez <mendezadrian149@gmail.com>
2021-09-17 21:24:19 -0400

    working on initial configuration

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+app.listen(process.env.APP_PORT, () => {
+  console.info(`Server is running on http://localhost:${process.env.APP_PORT}`)
+})
